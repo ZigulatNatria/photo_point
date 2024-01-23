7e58827ed1acd8eae7995299f9bc1c4406a3f54a
ZigulatNatria <vachrameev.oleg@yandex.ru>
2024-01-24 00:44:21 +0500

    Сохранение и вывод данных

--- photo_point/rate/views.py
+++ photo_point/rate/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import History
 
 
 def index(request):
     response_data = {}
-    response_data['result'] = 'error'
-    response_data['message'] = 'Some error message'
+    # response_data['result'] = 'error'
+    # response_data['message'] = 'Some error message'
+    all_history = History.objects.all()
+    for i in all_history:
+        response_data[f'reguest{i.id}'] = i.history
     return JsonResponse(response_data)
